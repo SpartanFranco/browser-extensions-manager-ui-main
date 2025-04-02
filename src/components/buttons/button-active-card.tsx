fa8c86842ec3adea9026991d90ed02c2ddda28a3
Geovani <geovanispartaco@gmail.com>
2025-04-01 21:34:30 -0400

    update

--- src/components/buttons/button-active-card.tsx
+++ src/components/buttons/button-active-card.tsx
@@ -17,7 +17,7 @@ export default function ButtonActiveCard({ isActive, name }: Props) {
 
 	return (
 		<button
-			className={`bg-opacity-20 flex h-[22px] w-[40px] cursor-pointer items-center rounded-full p-[3px] ${
+			className={`flex h-[22px] w-[40px] cursor-pointer items-center rounded-full p-[3px] ${
 				!isOn
 					? 'justify-start bg-(--Neutral-300) dark:bg-(--Neutral-600)'
 					: 'justify-end bg-(--Red-700) dark:bg-(--Red-500)'
