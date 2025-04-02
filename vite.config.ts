31e5a37211cd2a7ab0d76884d81c429518af4d7a
Geovani <geovanispartaco@gmail.com>
2025-04-01 20:04:13 -0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	base: 'https://SpartanFranco.github.io/browser-extensions-manager-ui-main.git',
+	base: 'https://SpartanFranco.github.io/browser-extensions-manager-ui-main',
 });
